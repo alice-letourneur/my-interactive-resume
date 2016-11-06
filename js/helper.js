02373fe8b3d4234ade77c8fee52cc7e696037f8f
“Alice <aliceletourneur@icloud.com>
2016-11-06 18:54:08 +0000

    Finalise project 2 using for loops and improving style to match portfolio

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<p>%data%</p><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -24,39 +24,44 @@ var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLwelcomeMsg = '<span class="welcome-message center-text">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3>';
+var HTMLskills = '<li class="flex-item"><img class="white-text logo-skills" src="%data%"></li>';
 
-var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkStart = '<div class="work-entry col-xs-12 col-md-6"></div>';
+var HTMLworkThumbnail = '<div class="thumbnail"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDates = '<div class="date-text"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>%data%</div>';
+var HTMLworkLocation = '<div class="location-text"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span>%data%</div>';
+var HTMLworkDescription = '<p><br><br>%data%</p>';
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectStart = '<div class="project-entry col-xs-12 col-md-6"></div>';
+var HTMLprojectThumbnail = '<div class="thumbnail"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectDates = '<div class="date-text"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>%data%</div>';
+var HTMLprojectDescription = '<p><br><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolStart = '<div class="education-entry col-xs-12 col-md-6"></div>';
+var HTMLschoolThumbnail = '<div class="thumbnail"></div>';
 var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolDates = '<div class="date-text"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>%data%</div>';
+var HTMLschoolLocation = '<div class="location-text"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span>%data%</div>';
+var HTMLschoolMajor = '<em><br><br>Major: %data%</em>';
 
+var HTMLonlineStart = '<div class="online-entry col-xs-12 col-md-6"></div>';
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClassesThumbnail = '<div class="thumbnail"></div>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineDates = '<div class="date-text"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>%data%</div>';
+var HTMLonlineURL = '<br><br><a href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
-var googleMap = '<div id="map"></div>';
+var googleMap = '<div id="map" class="col-xs-12"></div>';
 
 
 /*
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,72 +3,71 @@ var bio = {
 		"name": "Alice Letourneur",
 		"role": "Front-End Developer",
 		"contacts": {
-			"mobile": "07447 552714",
+			"mobile": "(+44) 07447 552714",
 			"email": "aliceletourneur@icloud.com",
 			"github": "alice-letourneur",
 			"location": "Brighton, UK"
 		},
-		"welcome_message": "Welcome to my bio",
+		"welcome_message": "Hi ! I'm Alice, a 25 yo Front-End Developer looking into  My description goes here. My description goes here. My description goes here. My description goes here. My description goes here. My description goes here. My description goes here. My description goes here. My description goes here. My description goes here. My description goes here.",
 		"skills": [
-			"reliable", "dedicated", "curious"
+			"images/logo-html5.svg", "images/logo-css3.svg", "images/logo-bootstrap.svg", "images/logo-javascript.svg", "images/logo-jquery.svg", "images/logo-git.svg"
 		],
 		"bioPic": "images/aliceletourneur.png"
 	}
 };
-	
 
-var formattedName =
-	HTMLheaderName.replace("%data%", bio.header.name);
-
-var formattedRole =
-	HTMLheaderRole.replace("%data%", bio.header.role);
-
-var formattedMobile = 
-	HTMLmobile.replace("%data%", bio.header.contacts.mobile);
-
-var formattedEmail = 
-	HTMLemail.replace("%data%", bio.header.contacts.email);
-
-var formattedGithub = 
-	HTMLgithub.replace("%data%", bio.header.contacts.github);
-
-var formattedLocation = 
-	HTMLlocation.replace("%data%", bio.header.contacts.location);
-
-var formattedWelcomeMessage = 
-	HTMLwelcomeMsg.replace("%data%", bio.header.welcome_message);
-
-
-
-var formattedPicture = 
-	HTMLbioPic.replace("%data%", bio.header.bioPic);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-$("#topContacts").prepend(formattedMobile);
-$("#topContacts").prepend(formattedEmail);
-$("#topContacts").prepend(formattedGithub);
-$("#topContacts").prepend(formattedLocation);
-$("#header").append(formattedPicture);
-$("#header").append(formattedWelcomeMessage);
-
-if (bio.header.skills.length > 0) {
+///This is an encapsulation
+bio.display = function() {
+	var formattedName =
+		HTMLheaderName.replace("%data%", bio.header.name);
+	var formattedRole =
+		HTMLheaderRole.replace("%data%", bio.header.role);
+	var formattedMobile = 
+		HTMLmobile.replace("%data%", bio.header.contacts.mobile);
+	var formattedEmail = 
+		HTMLemail.replace("%data%", bio.header.contacts.email);
+	var formattedGithub = 
+		HTMLgithub.replace("%data%", bio.header.contacts.github);
+	var formattedLocation = 
+		HTMLlocation.replace("%data%", bio.header.contacts.location);
+	var formattedWelcomeMessage = 
+		HTMLwelcomeMsg.replace("%data%", bio.header.welcome_message);
+	var formattedPicture = 
+		HTMLbioPic.replace("%data%", bio.header.bioPic);
+	var skillsList = '<ul id="skills" class="flex-box list-inline list-unstyled"></ul>';
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+	$("#topContacts").prepend(formattedMobile);
+	$("#topContacts").prepend(formattedEmail);
+	$("#topContacts").prepend(formattedGithub);
+	$("#topContacts").prepend(formattedLocation);
+	$("#header").append(formattedPicture);
+	$("#header").append(formattedWelcomeMessage);
 	$("#header").append(HTMLskillsStart);
-
-	var formattedSkills = 
-		HTMLskills.replace("%data%", bio.header.skills[0]);
-	$("#header").append(formattedSkills);
-
-	formattedSkills = 
-		HTMLskills.replace("%data%", bio.header.skills[1]);
-	$("#header").append(formattedSkills);
-
-	formattedSkills = 
-		HTMLskills.replace("%data%", bio.header.skills[2]);
-	$("#header").append(formattedSkills);
+	$("#header").append(skillsList);
+	if (bio.header.skills.length > 0) {
+		var formattedSkills = 
+			HTMLskills.replace("%data%", bio.header.skills[0]);
+		$("#skills").append(formattedSkills);
+		formattedSkills = 
+			HTMLskills.replace("%data%", bio.header.skills[1]);
+		$("#skills").append(formattedSkills);
+		formattedSkills = 
+			HTMLskills.replace("%data%", bio.header.skills[2]);
+		$("#skills").append(formattedSkills);
+		formattedSkills = 
+			HTMLskills.replace("%data%", bio.header.skills[3]);
+		$("#skills").append(formattedSkills);
+		formattedSkills = 
+			HTMLskills.replace("%data%", bio.header.skills[4]);
+		$("#skills").append(formattedSkills);
+		formattedSkills = 
+			HTMLskills.replace("%data%", bio.header.skills[5]);
+		$("#skills").append(formattedSkills);
+	};
 };
 
-
+bio.display();
 
 var work = { 
 	"jobs": [
@@ -77,7 +76,7 @@ var work = {
 		"employer": "American Express",
 		"workdates": "2014 - now",
 		"worklocation": "Brighton, UK",
-		"workdescription" : "My main focus is to ensure operational alignment and readiness for new product launches, processes or system changes and card member communications. I’ll have to ensure that the changes won't impact customer satisfaction, won’t drive unnecessary call volumes, won’t increase call handling time metrics and that CCPs have all the information required to be able to provide the best customer service. I am partnering closely across lines of business to prevent defects, record them, facilitate their resolution and ensure communication and awareness of CEN. Besides, I’ve participated to the ECCO Rationalization Project to increase accuracy of the content and optimized search results to build a trustworthy and reliable tool for our CCPs."
+		"workdescription" : "My main focus is to ensure operational alignment and readiness for new product launches, processes or system changes and card member communications. I’ll have to ensure that the changes won't impact customer satisfaction, won’t drive unnecessary call volumes, won’t increase call handling time metrics and that CCPs have all the information required to be able to provide the best customer service. I am partnering closely across lines of business to prevent defects, record them, facilitate their resolution."
 	},
 	{
 		"jobtitle": "Global New Accounts Subject Matter Expert",
@@ -89,9 +88,12 @@ var work = {
 	]
 };
 
-function displayWork() {
-	for (jobs in work.jobs) {
+///This is an encapsulation
+work.display = function() {
+		for (var jobs = 0; jobs < work.jobs.length; jobs++) {
 		$("#workExperience").append(HTMLworkStart);
+		$(".work-entry:last").append(HTMLworkThumbnail);
+
 		
 		var formattedEmployer = 
 		HTMLworkEmployer.replace("%data%", work.jobs[jobs].employer);
@@ -102,7 +104,7 @@ function displayWork() {
 		var formattedEmployerJobTitle =
 		formattedEmployer + formattedJobTitle;
 
-		$(".work-entry:last").append(formattedEmployerJobTitle);
+		$(".thumbnail:last").append(formattedEmployerJobTitle);
 
 		var formattedWorkDates = 
 		HTMLworkDates.replace("%data%", work.jobs[jobs].workdates);
@@ -112,16 +114,61 @@ function displayWork() {
 
 		var formattedWorkDatesLocation = formattedWorkDates + formattedWorkLocation;
 
-		$(".work-entry:last").append(formattedWorkDatesLocation);
+		$(".thumbnail:last").append(formattedWorkDatesLocation);
 
 		var formattedWorkDescription = 
 		HTMLworkDescription.replace("%data%", work.jobs[jobs].workdescription);
 		
-		$(".work-entry:last").append(formattedWorkDescription);
+		$(".thumbnail:last").append(formattedWorkDescription);
 	};
 };
 
-displayWork();
+work.display();
+
+var project = {
+	"projects": [
+	{
+		"projecttitle": "Classic Arcade Game Clone",
+		"projectdate": "Ongoing",
+		"projectdescription" : "I have used HTML5 and CSS3 to develop a responsive website to display the portfolio projects I will build!"
+	},
+	{
+		"projecttitle": "My Online Resume",
+		"projectdate": "November 2016",
+		"projectdescription" : "Create an online resume using HTML5, CSS3, JavaScript, and jQuery."
+	},
+	{
+		"projecttitle": "My Portfolio",
+		"projectdate": "October 2016",
+		"projectdescription" : "I have used HTML5 and CSS3 to develop a responsive website to display the portfolio projects I will build!"
+	}
+	]
+};
+
+///This is an encapsulation
+project.display = function() {
+		for (var projects = 0; projects < project.projects.length; projects++) {
+		$("#projects").append(HTMLprojectStart);
+		$(".project-entry:last").append(HTMLprojectThumbnail);
+
+		var formattedProjectTitle = 
+		HTMLprojectTitle.replace("%data%", project.projects[projects].projecttitle);
+		
+		$(".thumbnail:last").append(formattedProjectTitle);
+
+		var formattedProjectDates = 
+		HTMLprojectDates.replace("%data%", project.projects[projects].projectdate);
+
+		$(".thumbnail:last").append(formattedProjectDates);
+
+		var formattedProjectDescription =
+		HTMLprojectDescription.replace("%data%", project.projects[projects].projectdescription);
+
+		$(".thumbnail:last").append(formattedProjectDescription)
+	};
+};
+
+project.display();
 
 var education = { 
 	"schools": [
@@ -130,7 +177,7 @@ var education = {
 		"schooldates": "2009 - 2012",
 		"schoollocation": "Caen, Lower Normandy, FRANCE",
 		"schooldegree": "Bachelor",
-		"schoolmajor" : "Foreign Languages with Economics & International Business"
+		"schoolmajor" : "Foreign Languages (French, English, Italian, Spanish) with Economics & International Business"
 	},
 	{
 		"schoolname": "Lycée Pierre & Marie Curie",
@@ -145,14 +192,17 @@ var education = {
 			"onlineschool": "Udacity",
 			"onlinedates": "2016 - Today",
 			"onlinetitle": "Front-End Development Nanodegree",
+			"onlinedescription": "Through a project-based approach combined with code reviews from professional developers, the program teaches the fundamentals of how the web works and provides a working knowledge of the three foundational languages that power each and every website: HTML, CSS and JavaScript. The program leads you to being job-ready for an entry-level job as a Front-End Web Developer. Skills developed: Responsive Web Design Fundamentals, Responsive Images, jQuery, Object-Oriented JavaScript, HTML5 Canvas, Browser Rendering and Website Performance Optimization, Chrome Dev Tools, MVC-MVVM, JavaScript Design Patterns, KnockOut.js, JavaScript testing with Jasmine, Version Control with Git and GitHub.",
 			"onlineurl" : "www.udacity.com"
 		}
 	]
 };
 
-function displaySchool() {
-	for (schools in education.schools) {
+///This is an encapsulation
+education.display = function() {
+	for (var schools = 0; schools < education.schools.length; schools++) {
 		$("#education").append(HTMLschoolStart);
+		$(".education-entry:last").append(HTMLschoolThumbnail);
 
 		var formattedSchoolName = 
 			HTMLschoolName.replace("%data%", education.schools[schools].schoolname);
@@ -163,88 +213,51 @@ function displaySchool() {
 		var formattedSchoolNameDegree = 
 		formattedSchoolName + formattedSchoolDegree;
 		
-		$(".education-entry:last").append(formattedSchoolNameDegree);
+		$(".thumbnail:last").append(formattedSchoolNameDegree);
 
 		var formattedSchoolDates = 
 			HTMLschoolDates.replace("%data%", education.schools[schools].schooldates);
 
-		$(".education-entry:last").append(formattedSchoolDates);
+		$(".thumbnail:last").append(formattedSchoolDates);
 
 		var formattedSchoolLocation = 
 			HTMLschoolLocation.replace("%data%", education.schools[schools].schoollocation);
 
-		$(".education-entry:last").append(formattedSchoolLocation);
+		$(".thumbnail:last").append(formattedSchoolLocation);
 		
 		var formattedSchoolMajor = 
 		HTMLschoolMajor.replace("%data%", education.schools[schools].schoolmajor);
 
-		$(".education-entry:last").append(formattedSchoolMajor);
+		$(".thumbnail:last").append(formattedSchoolMajor);
 	};
 		
 
 		for (onlineclasses in education.onlineclass) {
-		$("#education").append(HTMLonlineClasses);
-		$("#education").append(HTMLschoolStart);
+		$("#education").append(HTMLonlineStart);
+		$(".online-entry:last").append(HTMLonlineClasses);
+		$(".online-entry:last").append(HTMLonlineClassesThumbnail);
+		$(".thumbnail:last").append(HTMLschoolStart);
 		var formattedOnlineTitle = 
 		HTMLonlineTitle.replace("%data%", education.onlineclass[onlineclasses].onlinetitle);
 		var formattedOnlineSchool = 
 		HTMLonlineSchool.replace("%data%", education.onlineclass[onlineclasses].onlineschool);		
 		var formattedOnlineTitleOnlineSchool = (formattedOnlineTitle + formattedOnlineSchool);
-		$(".education-entry:last").append(formattedOnlineTitleOnlineSchool);
+		$(".thumbnail:last").append(formattedOnlineTitleOnlineSchool);
 
 		var formattedOnlineDates = 
 		HTMLonlineDates.replace("%data%", education.onlineclass[onlineclasses].onlinedates);
 
-		$(".education-entry:last").append(formattedOnlineDates);
+		$(".thumbnail:last").append(formattedOnlineDates);
 
 		var formattedOnlineURL = 
 		HTMLonlineURL.replace("%data%", education.onlineclass[onlineclasses].onlineurl);
 
-		$(".education-entry:last").append(formattedOnlineURL);
+		$(".thumbnail:last").append(formattedOnlineURL);
 	};
 };
 
-displaySchool();
+education.display();
 
-
-var project = {
-	"projects": [
-	{
-		"projecttitle": "My Online Resume",
-		"projectdate": "October 2016",
-		"projectdescription" : "My description goes here"
-	},
-	{
-		"projecttitle": "My Portfolio",
-		"projectdate": "October 2016",
-		"projectdescription" : "My description goes here"
-	}
-	]
-};
-
-///This is an encapsulation
-project.display = function() {
-		for (projects in project.projects) {
-		$("#projects").append(HTMLprojectStart);
-		
-		var formattedProjectTitle = 
-		HTMLprojectTitle.replace("%data%", project.projects[projects].projecttitle);
-		
-		$(".project-entry:last").append(formattedProjectTitle);
-
-		var formattedProjectDates = 
-		HTMLprojectDates.replace("%data%", project.projects[projects].projectdate);
-
-		$(".project-entry:last").append(formattedProjectDates);
-
-		var formattedProjectDescription =
-		HTMLprojectDescription.replace("%data%", project.projects[projects].projectdescription);
-
-		$(".project-entry:last").append(formattedProjectDescription)
-	};
-};
-
-project.display();
 
 //Give me the click location on the page
 $(document).click(function(loc) {
@@ -253,16 +266,16 @@ $(document).click(function(loc) {
   logClicks (x,y);
 });
 
-$("#main").append(internationalizeButton);
+// $("#main").append(internationalizeButton);
 
-//Capitalize my Lastname
-var inName = function(name) {
-	name = name.split(" ");
-	name[1] = name[1].toUpperCase();
-	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+// //Capitalize my Lastname
+// var inName = function(name) {
+// 	name = name.split(" ");
+// 	name[1] = name[1].toUpperCase();
+// 	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
 
-	return name[0]+" "+name[1];
-};
+// 	return name[0]+" "+name[1];
+// };
 
 //Display map
 $("#mapDiv").append(googleMap);
